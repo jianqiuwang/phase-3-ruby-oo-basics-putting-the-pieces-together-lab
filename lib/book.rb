3fec599500c3dfb34426cf2f8d852022397e092b
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-10-05 19:47:42 -0500

    commit tree 45a35bcaa5937c7dd661dd565575a5b9bebdfd71

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    attr_accessor :genre, :title, :page_count, :author,:turn_page
+    def initialize(title)
+        @title=title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
